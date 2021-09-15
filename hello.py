371728de57d6ef8d58b51a3669a5c05f04c1136e
Sullen-ui <maorlov.it@gmail.com>
2021-09-15 23:12:07 +0700

    Second commit

--- hello.py
+++ hello.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<h1>Hello,  faka makaka fuckup!</h1>'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username 
